0f3ee86d4db5b417ad6e6080e1a651442e16c72b
y1j2x34 <y1j2x34@qq.com>
2024-11-23 22:48:24 +0800

    test code generator and field name convertor

--- propify-processor/src/test/java/com/vgerbot/propify/Utils_ConvertToFieldNameTest.java
+++ propify-processor/src/test/java/com/vgerbot/propify/Utils_ConvertToFieldNameTest.java
@@ -7,10 +7,10 @@ public class Utils_ConvertToFieldNameTest {
     @Test
     public void testConvertToFieldName_EmptyInput_ThrowsIllegalArgumentException() {
         try {
-            com.vgerbot.propify.Utils.convertToFieldName("");
+            Utils.convertToFieldName("");
             assert false;
         } catch (IllegalArgumentException e) {
-            assertEquals("Field name cannot be empty", e.getMessage());
+            assertEquals("Field name cannot be null or empty", e.getMessage());
         }
     }
 
@@ -18,34 +18,86 @@ public class Utils_ConvertToFieldNameTest {
     public void testConvertToFieldName_SimpleInput_ReturnsExpectedFieldName() {
         String input = "helloWorld";
         String expected = "helloWorld";
-        assertEquals(expected, com.vgerbot.propify.Utils.convertToFieldName(input));
+        assertEquals(expected, Utils.convertToFieldName(input));
     }
 
     @Test
     public void testConvertToFieldName_InputWithNonLetterOrDigit_ReturnsExpectedFieldName() {
         String input = "hello-World";
-        String expected = "hello_World";
-        assertEquals(expected, com.vgerbot.propify.Utils.convertToFieldName(input));
+        String expected = "helloWorld";
+        assertEquals(expected, Utils.convertToFieldName(input));
     }
 
     @Test
     public void testConvertToFieldName_InputStartingWithDigit_ReturnsExpectedFieldName() {
         String input = "123hello";
         String expected = "_123hello";
-        assertEquals(expected, com.vgerbot.propify.Utils.convertToFieldName(input));
+        assertEquals(expected, Utils.convertToFieldName(input));
     }
 
     @Test
     public void testConvertToFieldName_InputWithReservedKeyword_ReturnsExpectedFieldName() {
         String input = "class";
         String expected = "class_";
-        assertEquals(expected, com.vgerbot.propify.Utils.convertToFieldName(input));
+        assertEquals(expected, Utils.convertToFieldName(input));
     }
 
     @Test
     public void testConvertToFieldName_InputWithMultipleWords_ReturnsExpectedFieldName() {
         String input = "hello world";
-        String expected = "hello_World";
-        assertEquals(expected, com.vgerbot.propify.Utils.convertToFieldName(input));
+        String expected = "helloWorld";
+        assertEquals(expected, Utils.convertToFieldName(input));
+    }
+
+    @Test
+    public void testConvertToFieldName_ConsecutiveSpecialChars_ReturnsSingleUnderscore() {
+        String input = "hello---world";
+        String expected = "helloWorld";
+        assertEquals(expected, Utils.convertToFieldName(input));
+    }
+
+    @Test
+    public void testConvertToFieldName_TrailingSpecialChars_RemovesTrailingUnderscore() {
+        String input = "hello---";
+        String expected = "hello";
+        assertEquals(expected, Utils.convertToFieldName(input));
+    }
+
+    @Test
+    public void testConvertToFieldName_DollarSign_PreservesDollarSign() {
+        String input = "hello$world";
+        String expected = "hello$world";
+        assertEquals(expected, Utils.convertToFieldName(input));
+    }
+
+    @Test
+    public void testConvertToFieldName_UnicodeLetters_PreservesUnicodeLetters() {
+        String input = "helloワールド";
+        String expected = "helloワールド";
+        assertEquals(expected, Utils.convertToFieldName(input));
+    }
+
+
+    @Test
+    public void testConvertToFieldName_AllSpecialChars_ReturnsUnderscore() {
+        String input = "!@#$%^&*()";
+        String expected = "_$";
+        assertEquals(expected, Utils.convertToFieldName(input));
+    }
+
+    @Test
+    public void testConvertToFieldName_MoreJavaKeywords_ReturnsWithUnderscore() {
+        String[] keywords = {"if", "for", "while", "do", "break", "continue", "return", 
+                           "void", "int", "boolean", "double", "float", "long"};
+        for (String keyword : keywords) {
+            assertEquals(keyword + "_", Utils.convertToFieldName(keyword));
+        }
+    }
+
+    @Test
+    public void testConvertToFieldName_MixedCase_PreservesCase() {
+        String input = "helloWORLDtest";
+        String expected = "helloWORLDtest";
+        assertEquals(expected, Utils.convertToFieldName(input));
     }
 }
